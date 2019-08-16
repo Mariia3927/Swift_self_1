e000d6e93e8df0ea0c4345b23afe6843a4a1e30a
Mariia Shestakova <48029085+Mariia3927@users.noreply.github.com>
2019-08-16 09:24:54 +0300

    Add files via upload

---
+++ Swift_Self/Data.swift
@@ -0,0 +1,19 @@
+//
+//  Data.swift
+//  Swift_Self
+//
+//  Created by MariyaShestakova on 8/11/19.
+//  Copyright © 2019 MariyaShestakova. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+struct Data {
+    var _text: String
+    var _state: Bool
+    var _window: NSWindow
+}
+
+
+
